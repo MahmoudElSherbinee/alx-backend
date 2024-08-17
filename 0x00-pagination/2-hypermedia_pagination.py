465f066af7537bde8febe96c5454a93272d118b8
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2024-08-17 20:20:45 +0000

    fix 2

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
-""" Pagination """
-
-
+"""Hypermedia pagination sample.
+"""
 import csv
 import math
-from typing import List, Tuple
+from typing import Dict, List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Retrieves the index range from a given page and page size.
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
 
 
 class Server:
@@ -13,6 +20,8 @@ class Server:
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """Initializes a new Server instance.
+        """
         self.__dataset = None
 
     def dataset(self) -> List[List]:
@@ -26,66 +35,91 @@ class Server:
 
         return self.__dataset
 
-    def index_range(self, page: int, page_size: int) -> Tuple[Tuple]:
-        """ Function that reads the number of a page
-        page: (int) - number of page
-        page_size: (int) - total page size
-
-        return: (tuple)
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Retrieves a page of data.
         """
-        self.page = page
-        self.page_size = page_size
-        if self.page <= 0:
-            return None
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        if start > len(data):
+            return []
+        return data[start:end]
 
-        if self.page_size == 0:
-            return (self.page, self.page_size)
-            # I can instead return page and page
-        page_start = (self.page - 1) * self.page_size
-        page_end = page_start + self.page_size
-        return (page_start, page_end)
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """Retrieves information about a page.
+        """
+        page_data = self.get_page(page, page_size)
+        start, end = index_range(page, page_size)
+        total_pages = math.ceil(len(self.__dataset) / page_size)
+        page_info = {
+            'page_size': len(page_data),
+            'page': page,
+            'data': page_data,
+            'next_page': page + 1 if end < len(self.__dataset) else None,
+            'prev_page': page - 1 if start > 0 else None,
+            'total_pages': total_pages,
+        }
+        return page_info#!/usr/bin/env python3
+"""Hypermedia pagination sample.
+"""
+import csv
+import math
+from typing import Dict, List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Retrieves the index range from a given page and page size.
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        """Initializes a new Server instance.
+        """
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """ Get page function """
-        assert isinstance(page, int) and page > 0
-        # assert isinstance(self.page, int) and isinstance(self.page_size, int)
-        assert isinstance(page_size, int) and page_size > 0
-        dataset = self.dataset()
-
-        try:
-            pages, pagesize = self.index_range(page, page_size)
-            page_line = dataset[pages:pagesize]
-        except IndexError:
+        """Retrieves a page of data.
+        """
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        if start > len(data):
             return []
+        return data[start:end]
 
-        return page_line
-
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """ A function that returns dictionary of list """
-        next_page = 0
-        prev_page = 0
-        total_page = round(int(19419 / page_size))
-        if page <= 1:
-            prev_page = None
-
-        if page > 1:
-            prev_page = page - 1
-
-        if page > total_page:
-            next_page = None
-
-        else:
-            next_page = page + 1
-
-        # page_sizes = 19419 / total_page
-
-        diction = {
-            'page_size': page_size,
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """Retrieves information about a page.
+        """
+        page_data = self.get_page(page, page_size)
+        start, end = index_range(page, page_size)
+        total_pages = math.ceil(len(self.__dataset) / page_size)
+        page_info = {
+            'page_size': len(page_data),
             'page': page,
-            'data': self.get_page(
-                page,
-                page_size),
-            'next_page': next_page,
-            'prev_page': prev_page,
-            'total_pages': total_page}
-        return diction
+            'data': page_data,
+            'next_page': page + 1 if end < len(self.__dataset) else None,
+            'prev_page': page - 1 if start > 0 else None,
+            'total_pages': total_pages,
+        }
+        return page_info
